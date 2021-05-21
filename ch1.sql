7970f9c7923a88c8e8592b264eb8953c0f2e1a72
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-05-21 19:55:22 +0200

    Adding data to the table

--- ch1.sql
+++ ch1.sql
@@ -1,8 +1,12 @@
-/* This is a comment */
+/* This is a comment
+
+The table below is missing several constraints
+
+*/
 
 CREATE TABLE teachers (
-	id bigserial,
-	first_name varchar(25),
+	id bigserial, /* bigserial = a special data type, that auto-increments everytime you add a row. E.g., adding value 1 to each id column */
+	first_name varchar(25), /* These are the column names, with the max allowed length*/
 	last_name varchar(50),
 	school varchar(50),
 	hire_date date,
--- first_table.sql
+++ first_table.sql
@@ -1,10 +1,25 @@
-/* This is a comment */
+/* This is a comment
+
+The table below is missing several constraints
+
+Step 1. Create the table: 
+
 
 CREATE TABLE teachers (
-	id bigserial,
-	first_name varchar(25),
+	id bigserial, /* bigserial = a special data type, that auto-increments everytime you add a row. E.g., adding value 1 to each id column */
+	first_name varchar(25), /* These are the column names, with the max allowed length*/
 	last_name varchar(50),
 	school varchar(50),
 	hire_date date,
 	salary numeric
 );
+--------------------------------- */
+
+/* Step 2: Inserting rows/data into the table: */
+INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
+VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
+	('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-05-22', 65000),
+	('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500),
+	('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200),
+	('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
+	('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
