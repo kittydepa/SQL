a427c9a7f97323b9119e1ed36c4fa30673ba44c6
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-04 11:10:40 +0200

    Ch 3 Ex 2 - initial commit

--- ch3_ex.sql
+++ ch3_ex.sql
@@ -1,5 +1,20 @@
 /* Ch 3. Examples */
 
+/* Ex. 3.2 -  Exploring different numeric types*/
+CREATE TABLE number_data_types (
+	numeric_column numeric(20,5),
+	real_column real,
+	double_column double precision
+);
+
+INSERT INTO number_data_types
+VALUES
+	(.7, .7, .7)
+	(2.13579, 2.13579, 2.13579),,
+	(2.1357987654, 2.1357987654, 2.1357987654);
+SELECT * FROM number_data_types;
+
+
 /* Step one for example 3.1. 
 
 CREATE TABLE char_data_types (
@@ -13,6 +28,7 @@ VALUES
 	('abc', 'abc', 'abc'),
 	('defghi', 'defghi', 'defghi'); */
 
-/* Step 2 for example 3.1 */
+/* Step 2 for example 3.1
 COPY char_data_types TO 'C:\Users\Public\typetest.txt'  /* Needed to be somewhere public...?? */
-WITH (FORMAT CSV, HEADER, DELIMITER ','); 
+WITH (FORMAT CSV, HEADER, DELIMITER ','); */
+
