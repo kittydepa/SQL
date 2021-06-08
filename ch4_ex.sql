efe53fa09f3bbc8860a8d5837b36e5badc10d7f3
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-08 19:46:30 +0200

    Adding a CSV file for examples

--- ch4_ex.sql
+++ ch4_ex.sql
@@ -5,4 +5,5 @@ COPY table_name     - note that the table needs to be one that already exists in
 					- can read this as 'Copy data to my table called table_name' 
 FROM 'C:\YourDirectory\your_file.csv'
 WITH (FORMAT CSV, HEADER);
+					- says to exclude the HEADER row upong import
 */
