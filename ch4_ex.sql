fdf75f33566053fbf8ddb2d39b8fc00b5aaa3832
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-08 17:15:05 +0200

    Ch 4 importing source CSV data to SQL

--- ch4_ex.sql
+++ ch4_ex.sql
@@ -1,1 +1,8 @@
 /* Ch 4 Examples */
+
+/* How to import data from a source CSV file into SQL:
+COPY table_name     - note that the table needs to be one that already exists in the database
+					- can read this as 'Copy data to my table called table_name' 
+FROM 'C:\YourDirectory\your_file.csv'
+WITH (FORMAT CSV, HEADER);
+*/
